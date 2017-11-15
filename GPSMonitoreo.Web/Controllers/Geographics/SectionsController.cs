9376a089dbc975d4325d20618e42f59d71ddd399
vicente.pinargote <vicente.pinargote@LILIANA-CALDERO>
2017-11-15 16:11:18 -0500

    1

--- GPSMonitoreo.Web/Controllers/Geographics/SectionsController.cs
+++ GPSMonitoreo.Web/Controllers/Geographics/SectionsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 using GPSMonitoreo.Services;
 using MVCHelpers.ActionResults;
 using GPSMonitoreo.Web.ViewComponents.jqxGrid;
